0006445889e41e76bc342ea1f6a400d80798c9ed
tran ha thu <hathu2516@gmail.com>
2023-01-18 20:07:44 +0800

    modified:   src/components/Navbar/navbar.js

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav style={{display: 'flex', justifyContent: 'flex-start'}}>
-            <p className='f3 link dim black underline pa3 pointer'>Hello</p>
-            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-                <p className='f3 link dim black underline pa3 pointer'>Sign In</p>
-                <p className='f3 link dim black underline pa3 pointer'>Register</p>
-            </nav>
+        <nav class="db dt-l w-100 border-box pa3 ph5-l">
+            <a class="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#" title="Home">
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" class="dib w2 h2 br-100" alt="CVWO"/>
+                <title class="dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Home">Home</title>
+            </a>
+            <div class="db dtc-l v-mid w-100 w-75-l tc tr-l">
+                <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Home">Home</a>
+                <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="About">About</a>
+                <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Profile">Profile</a>
+            </div>
         </nav>
     )
 }
